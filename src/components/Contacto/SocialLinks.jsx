c3b491ae4caa62e1a8b74f343bec4bc649a0a29a
Nn3z <ddaevir19@icloud.com>
2025-08-11 12:24:36 -0600

    add social links

--- src/components/Contacto/SocialLinks.jsx
+++ src/components/Contacto/SocialLinks.jsx
@@ -5,13 +5,13 @@ import { FaGithub, FaLinkedin, FaTwitter, FaInstagram } from "react-icons/fa";
 export default function SocialLinks() {
   return (
     <div className="social-links">
-      <a href="https://github.com/tuusuario" target="_blank" rel="noopener noreferrer">
+      <a href="https://github.com/Nn3z" target="_blank" rel="noopener noreferrer">
         <FaGithub />
       </a>
-      <a href="https://linkedin.com/in/tuusuario" target="_blank" rel="noopener noreferrer">
+      <a href="https://www.linkedin.com/in/pablo-ponce-82ab61379/" target="_blank" rel="noopener noreferrer">
         <FaLinkedin />
       </a>
-      <a href="https://instagram.com/tuusuario" target="_blank" rel="noopener noreferrer">
+      <a href="https://www.instagram.com/ponce_wd?igsh=MW9uZGV6MTdhZzZkcg%3D%3D&utm_source=qr" target="_blank" rel="noopener noreferrer">
         <FaInstagram />
       </a>
     </div>
