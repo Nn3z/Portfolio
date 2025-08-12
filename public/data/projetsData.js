c18e089761d0ecb8f21377a13587be0650a45dd0
Nn3z <ddaevir19@icloud.com>
2025-08-11 18:56:14 -0600

    add project in data

--- public/data/projetsData.js
+++ public/data/projetsData.js
@@ -4,5 +4,10 @@ export const projects = [
         description: "Este es un script hecho con bash, para la automatización de la instalación y configuración de un entorno de desarrollo en Go.",
         image: "/iconos/imgprojects/scriptGO.png",
         link: "https://github.com/Nn3z/script-create-go-back"
-    },
+    },{
+        name: "Portafolio",
+        description: "Mi portafolio personal, hecho con React y Vite, para mostrar mis proyectos y habilidades como desarrollador web.",
+        image: "/iconos/imgprojects/portfolio.png",
+        link: "https://diego-ponce.vercel.app/"
+    }
 ]
